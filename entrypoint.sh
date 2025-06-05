48cfb4287492d9d90e5a32040175426d02878b3f
manoj-astuto <manoj.v@astuto.ai>
2025-06-05 11:44:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check the deployment_type environment var
+# Check the deployment_type environment variable
 if [ "$deployment_type" = "job" ]; then
   SCRIPT_NAME="install.sh"
 elif [ "$deployment_type" = "cronjob" ]; then
