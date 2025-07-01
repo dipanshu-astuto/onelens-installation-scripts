923e4a587424c48922cc3decd2e396f3336ca567
manoj-astuto <manoj.v@astuto.ai>
2025-07-01 12:59:32 +0530

    Update patching.sh

--- patching.sh
+++ patching.sh
@@ -160,7 +160,8 @@ echo "helm upgrade onelens-agent onelens/onelens-agent with dynamic resource all
 
 helm repo add onelens https://astuto-ai.github.io/onelens-installation-scripts
 helm repo update
-helm rollback onelens-agent 1 -n onelens-agent
+REVISION=$(helm history onelens-agent -n onelens-agent | grep -v '02:00' | tail -1 | awk '{print $1}')
+helm rollback onelens-agent $REVISION -n onelens-agent
 
 # # Perform the upgrade with dynamically calculated resource values
 # helm upgrade onelens-agent onelens/onelens-agent \
